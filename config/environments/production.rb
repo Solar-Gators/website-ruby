b8672fe11ae418ae348e4d9a96c839eccea79fb4
Pablo Estrada <pablof300@gmail.com>
2019-02-14 15:21:00 -0500

    Changed some Heroku configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.serve_static_assets = true
+  config.assets.digest = true
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
